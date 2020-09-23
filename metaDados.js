d198e55b459ee49f5ce2d2fdabf1373b7604a86d
Italo Francisco Saraiva Ibiapina <iibiapina@toyota.com.br>
2020-09-23 15:26:38 -0300

    'Commit'

--- metaDados.js
+++ metaDados.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
     $('#'+compName).load('https://italoibiapina.github.io/code-base-mock/template-basico/login-form/login-form.html');
 
     let vTitulo = metaDados.body["login-form"].titulo;
-    let objt = $('#titulo', '#'+compName);
+    let objt = $('#titulo'/* , '#'+compName */);
     $(objt).html('Hello World');
 
   }
